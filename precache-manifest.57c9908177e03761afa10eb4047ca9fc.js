933e15617429aacc130d89bd5fdb52e59c092700
anthony <anthonyip@hotmail.fr>
2019-12-06 13:39:30 +0100

    Updates

---
+++ precache-manifest.57c9908177e03761afa10eb4047ca9fc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a92df8256fd9f68377a1b20e5b58ee34",
+    "url": "/monster-rolldex/index.html"
+  },
+  {
+    "revision": "4ab162a5bf007be8630e",
+    "url": "/monster-rolldex/static/css/main.3e790a0f.chunk.css"
+  },
+  {
+    "revision": "b329ae1d6927d397d9c3",
+    "url": "/monster-rolldex/static/js/2.8eea32d1.chunk.js"
+  },
+  {
+    "revision": "4ab162a5bf007be8630e",
+    "url": "/monster-rolldex/static/js/main.a5fdd3d4.chunk.js"
+  },
+  {
+    "revision": "43df3653b4befa65bc24",
+    "url": "/monster-rolldex/static/js/runtime-main.e885aa95.js"
+  }
+]);
